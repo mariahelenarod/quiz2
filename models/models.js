fa421171ad485ef96347a54c0d890f56cbb7520d
jotamontoyo <jotamontoyo@gmail.com>
2015-06-12 19:39:51 +0100

    quiz v1.2
    Se añade sqlite y sequelize. Despligue en Heroku

--- models/models.js
+++ models/models.js
@@ -5,9 +5,9 @@ var path = require('path');
 var Sequelize = require('sequelize');								// crea objeto de la clase modelo ORM
 
 var sequelize = new Sequelize(null, null, null, 					// Usar BBDD SQLite o Postgres. constructor del obejto tabla sequelize
-  { dialect:  'sqlite',
-    storage:  'quiz.sqlite'  // solo SQLite (.env)
-  }     
+	{ dialect:  'sqlite',
+      storage:  'quiz.sqlite'  											// solo SQLite (.env)
+  	}     
 );
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));			// importar estructura y definicion de la tabla Quiz
@@ -15,12 +15,12 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));			// importar estruct
 exports.Quiz = Quiz;												// exportar tablas
 
 sequelize.sync().then(function() {									// sequelize.sync() inicializa tabla de preguntas en DB
-	Quiz.count().success(function(count) {							// success ejecuta el manejador cuando crea la tabla. Quiz.count().success() devuelve el numero de filas
+	Quiz.count().then(function(count) {							// success ejecuta el manejador cuando crea la tabla. Quiz.count().success() devuelve el numero de filas
 		if (count === 0) {											// se inicializa solo si esta vacia
 			Quiz.create({
 				pregunta: DataTypes.STRING,
 		  		respuesta: DataTypes.STRING
-			}).success(function() {console.log('Base creada')});
+			}).then(function() {console.log('Base creada')});
 		};
 	});
 });
