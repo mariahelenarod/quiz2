379c479f61093fd421516a5aecd6ef39400880e4
jotamontoyo <jotamontoyo@gmail.com>
2015-06-14 21:02:11 +0100

    quiz 1.8
    Se añade autoload y control de errores mod07-5

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,37 +1,47 @@
 
 var models = require('../models/models.js');
 
-exports.question = function(req, res) {									// GET /quizes/question
+// autoload. solo se ejecuta si en la peticion GET existe un :quizId. ayuda a factorizar el codigo del resto de controladores 
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(						//si find() tiene exito
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quiz[id]));
+			}
+		}
+	).catch(function(error) {next(error);});
+};
+
+
+/* exports.question = function(req, res) {									// GET /quizes/question
 //	res.render('quizes/question', {pregunta: 'Capital de Italia'});		// renderiza /views/question.ejs con el objeto pregunta
 	models.Quiz.findAll().then(function(quiz) {							
 		res.render('quizes/question', {pregunta: quiz[0].pregunta})
 	})
-};
+}; */
 
-exports.index = function(req, res) {	
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes});				// findAll() renderiza toda la lista de preguntas que se genera en /quizes/index.ejs
-	})
+exports.index = function(req, res) {									// GET /quizes					
+	models.Quiz.findAll().then(
+		function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes});			// findAll() renderiza toda la lista de preguntas que se genera en /quizes/index.ejs
+		}
+	).catch(function(error) {next(error);})
 };
 
 exports.show = function(req, res) {										// GET /quizes/:id
-	models.Quiz.find(req.params.quizId).then(function(quiz) {			// find() seleciona en la tabla Quiz req.params.quizId y lo pasa como argumento quiz a la funcion
+//	models.Quiz.find(req.params.quizId).then(function(quiz) {			// find() seleciona en la tabla Quiz req.params.quizId y lo pasa como argumento quiz a la funcion
 		res.render('quizes/show', {quiz: quiz});						// renderiza la vista /quizes/show del quizId selecionado con find()
-	})									          						// req.quiz: instancia de quiz cargada con autoload
-};
+};									          						// req.quiz: instancia de quiz cargada con autoload
+
 	
 exports.answer = function(req, res) {									// GET /quizes/answer/:id
-	models.Quiz.find(req.params.quizId).then(function(quiz) {			
-		if (req.query.respuesta === quiz.respuesta) {					// comprueba la variable respuesta de la peticion GET req recibida del form question.ejs vs quiz.respuesta, que es la respuesta que devuelve find()
-			res.render('quizes/answer', 
-				{quiz: quiz, respuesta: 'Correcto'}						// renderiza /views/answer.ejs con el objeto quiz y respuesta
-			);		
-		} else {
-			res.render('quizes/answer', 
-				{quiz: quiz, respuesta: 'Incorrecto'}
-			);
-		};
-
-	})
+	var resultado = 'Incorrecto';			
+	if (req.query.respuesta === req.quiz.respuesta) {					// comprueba la variable respuesta de la peticion GET req recibida del form question.ejs vs req.quiz.respuesta, que es la respuesta que devuelve find() del autoload
+		resultado = 'Correcto';											// renderiza /views/answer.ejs con el objeto quiz y respuesta
+	};
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado};
 };
 
