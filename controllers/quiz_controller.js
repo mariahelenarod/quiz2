5a6d4d252d394983b832277130430ee9df52abfe
jotamontoyo <jotamontoyo@gmail.com>
2015-06-16 19:37:49 +0100

    quiz 2.1
    Se implementa añadir pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,6 @@
-
 var models = require('../models/models.js');
 
-// autoload. solo se ejecuta si en la peticion GET existe un :quizId. ayuda a factorizar el codigo del resto de controladores 
-exports.load = function(req, res, next, quizId) {
+exports.load = function(req, res, next, quizId) {		// autoload. solo se ejecuta si en la peticion GET existe un :quizId. ayuda a factorizar el codigo del resto de controladores 
 	models.Quiz.find(quizId).then(						//si find() tiene exito
 		function(quiz) {
 			if (quiz) {
@@ -15,15 +13,7 @@ exports.load = function(req, res, next, quizId) {
 	).catch(function(error) {next(error);});
 };
 
-
-/* exports.question = function(req, res) {									// GET /quizes/question
-//	res.render('quizes/question', {pregunta: 'Capital de Italia'});		// renderiza /views/question.ejs con el objeto pregunta
-	models.Quiz.findAll().then(function(quiz) {							
-		res.render('quizes/question', {pregunta: quiz[0].pregunta})
-	})
-}; */
-
-exports.index = function(req, res) {					// GET /quizes	
+exports.index = function(req, res) {										// GET /quizes	
 	var search = '%' + (req.query.search || '').replace(/ /g,'%') + '%';    // con req de la peticion GET monta la ruta + el valor introducido de req.query.search
 	if (req.query.search) {
 		models.Quiz.findAll({where: ["pregunta like ?", search], order:'pregunta ASC'}).then(    		// findAll() selecciona con sql
@@ -34,7 +24,7 @@ exports.index = function(req, res) {					// GET /quizes
 	} else {
 		models.Quiz.findAll().then(
 			function(quizes) {
-				res.render('quizes/index.ejs', {quizes: quizes});			// findAll() renderiza toda la lista de preguntas que se genera en /quizes/index.ejs
+				res.render('quizes/index.ejs', {quizes: quizes});		// findAll() renderiza toda la lista de preguntas que se genera en /quizes/index.ejs
 			}
 		).catch(function(error) {next(error);});
 	};
@@ -45,12 +35,17 @@ exports.show = function(req, res) {										// GET /quizes/:id
 		res.render('quizes/show', {quiz: req.quiz});					// renderiza la vista /quizes/show del quizId selecionado con load find()
 };									          							// req.quiz: instancia de quiz cargada con autoload
 
-	
 exports.answer = function(req, res) {									// GET /quizes/answer/:id
 	var resultado = 'Incorrecto';			
 	if (req.query.respuesta === req.quiz.respuesta) {					// comprueba la variable respuesta de la peticion GET req recibida del form question.ejs vs req.quiz.respuesta, que es la respuesta que devuelve find() del autoload
-		resultado = 'Correcto';											// renderiza /views/answer.ejs con el objeto quiz y respuesta
+		resultado = 'Correcto';											
 	};
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});// renderiza /views/answer.ejs con el objeto quiz y respuesta
 };
 
+exports.new = function(req, res) {										// GET /quizes/new
+	var quiz = models.Quiz.build( 										// crea el objeto quiz, lo construye con buid() que es un metodo de sequilize
+    	{pregunta: "Pregunta", respuesta: "Respuesta"}					// asigna literales a los campos pregunta y respuestas para que se vea el texto en el <input> cuando creemos el formulario
+  	);
+	res.render('quizes/new', {quiz: quiz, errors: []});					// renderiza la vista quizes/new
+};
--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'Quiz' });								// cuando renderice la vista index.ejs le pasa el objeto title: 'Quiz'
 });
 
-// router.get('/quizes/question', quizController.question);				// controla las peticiones GET a /quizes/question segun el enrutador quiz_controller.js
-// router.get('/quizes/answer', quizController.answer);					// controla las peticiones GET a /quizes/answer segun el enrutador quiz_controller.js
-
 // autoload de comandos con :quizId
 router.param('quizId', quizController.load);							// peticiones GET con SQL :quizId
 
-router.get('/quizes',			 				quizController.index);			// accede a la lista completa de preguntas
+router.get('/quizes',			 				quizController.index);			// accede a la lista completa de preguntas /quizes/index.ejs
 router.get('/quizes/:quizId(\\d+)',				quizController.show);			// accede a una pregunta en concreto. envia al quizController la peticion GET con el parametro quizId (indice)
 router.get('/quizes/:quizId(\\d+)/answer',		quizController.answer);			// se dispara cuando submit del form question.ejs hacia la ruta /quizes/answer. le pasa el id en la peticion GET req
+
+router.get('/quizes/new',						quizController.new);			// carga el formulario /quizes/new
+router.post('/quizes.create'),					quizController.create);			// carga el formulario /quizes/create cuando el boton <salvar> del formulario new
 
 router.get('/profile/author', function(req, res) {
 	res.render('profile/author', { title: 'Autor' });					// visualiza el autor
