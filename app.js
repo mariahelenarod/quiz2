d93327c32abcca9912579a09e0f90dd1a75d2d57
jotamontoyo <jotamontoyo@gmail.com>
2015-06-25 19:27:08 +0100

    control sesion

--- app.js
+++ app.js
@@ -43,7 +43,7 @@
 	
 	app.use(function(req, res, next) {
 		if (req.session.user) {
-			if (Date.now() - req.user.session.lastRequestTime > 1*60*1000) {
+			if (Date.now() - req.session.user.lastRequestTime > 1*60*1000) {
 				delete req.session.user;
 			} else {
 				req.user.session.lastRequestTime = Date.now();
