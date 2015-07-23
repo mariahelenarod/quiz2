0474948d6a0fe08441d0f4173ad385f9be54559f
jotamontoyo <jotamontoyo@gmail.com>
2015-07-23 20:25:52 +0100

    st

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -83,7 +83,7 @@
 			statistics.comments 			= results[1];
 			statistics.average_comments 	= (statistics.comments / statistics.quizes).toFixed(2);
 			for (index in results[2]) {
-				if (results[2][index].Comment) {
+				if (results[2][index].comment.length) {
 					statistics.commented_quizes++;
 				} else {
 					statistics.no_commented++;
