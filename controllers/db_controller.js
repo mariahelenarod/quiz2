3e0c376db6a56b46f374be9bf64a2c0f5e9df75a
jotamontoyo <jotamontoyo@gmail.com>
2015-07-29 17:58:28 +0100

    db

--- controllers/db_controller.js
+++ controllers/db_controller.js
@@ -4,12 +4,12 @@
 	exports.show = function(req, res, next) {
 		Promise.all([
 			models.Quiz.count(),
-			models.Comment.count(),
-			models.Quiz.findAll({
+			models.Comment.count()
+/*			models.Quiz.findAll({
 				include: [{
 					model: models.Comment
 				}]
-			})
+			}) */
 		]).then(function(results) {
 			res.render('db/index', {results: results, errors: []});
 
