052747bda951b6e65e60165312ba8e875f9bb8ee
jotamontoyo <jotamontoyo@gmail.com>
2015-07-30 19:08:54 +0100

    st

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -102,7 +102,7 @@
 	
 	exports.destroy = function(req, res) {
 		req.quiz.destroy().then(function() {
-			for (var i in req.quiz.comments) {
+			for (var i in req.quiz.comments) { 
 				req.quiz.comments[i].destroy();
 			};
 			res.redirect('/quizes');
